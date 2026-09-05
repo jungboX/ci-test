c277cbbab9d3f9e8889cde93c39ba41194be875f
jungboX <stonesys123@naver.com>
2026-09-06 02:35:35 +0900

    테스트 실패 유도

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello yerim!'; // 테스트 실패 유도
   }
 }
