241cb593c62cff163cd21df49c4b37ceb1f98e17
jungboX <stonesys123@naver.com>
2026-09-07 00:38:32 +0900

    서비스 멘트 변경

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!'; // 테스트 실패 유도
+    return 'Hello CI/CD!';
   }
 }
